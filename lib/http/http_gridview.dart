7f254382c5801bc47a592d03b7d0b6f2dcc27570
hongri <1285052668@qq.com>
2021-04-09 11:39:20 +0800

    gridview优化

--- lib/http/http_gridview.dart
+++ lib/http/http_gridview.dart
@@ -6,39 +6,29 @@
 
 //为了避免掉帧，像上面那样消耗性能的计算就应该放在后台处理。在 Android 平台上，
 // 这意味着你需要在不同的线程中进行调度工作。而在 Flutter 中，你可以使用一个单独的 Isolate。
-
 import 'dart:async';
 import 'dart:convert';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<List<Photo>> fetchPhotos(http.Client client) async {
-  final response =
-      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
-  //通过 Flutter 提供的 compute() 方法将解析和转换的工作移交到一个后台 isolate 中
-  //这个 compute() 函数可以在后台 isolate 中运行复杂的函数并返回结果。
-  return compute(parsePhotos, response.body);
-}
-
-List<Photo> parsePhotos(String responseBody) {
-  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
-  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
-}
-
-class IsolateApp extends StatelessWidget {
+/// 在写应用的过程中，取决于是否需要管理状态，
+/// 通常会创建一个新的组件继承 StatelessWidget 或 StatefulWidget。
+class GirdApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Isolate Demo';
-
+    final appTitle = 'GridView Demo';
     return MaterialApp(
       title: appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
       home: MyHomePage(title: appTitle),
     );
   }
 }
 
+///相当于ContentView
 class MyHomePage extends StatelessWidget {
   final String title;
 
@@ -50,20 +40,48 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
+
+      ///创建一个基于与[Future]交互的最新快照的一个Widget
       body: FutureBuilder<List<Photo>>(
         future: fetchPhotos(http.Client()),
         builder: (context, snapshot) {
-          if (snapshot.hasError) print(snapshot.error);
-
-          return snapshot.hasData
-              ? PhotosList(photos: snapshot.data)
-              : Center(child: CircularProgressIndicator());
+          if (snapshot.connectionState == ConnectionState.done) {
+            //future执行完毕
+            if (snapshot.hasData) {
+              return PhotosList(photos: snapshot.data);
+            } else if (snapshot.hasError) {
+              print(snapshot.error);
+            }
+          }
+          return Center(child: CircularProgressIndicator());
         },
       ),
     );
   }
 }
 
+///获取网络数据，并解析
+///此处Future相当于ES6的Promise, async/await相当于ES7中的async/await
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  final response =
+      await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+  ///通过 Flutter 提供的 compute() 方法将解析和转换的工作移交到一个后台 isolate 中
+  ///这个 compute() 函数可以在后台 isolate 中运行复杂的函数并返回结果。
+  if (response.statusCode == 200) {
+    return compute(parsePhotos, response.body);
+  } else {
+    print("failed to load data");
+  }
+  return null;
+}
+
+///json数据解析
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+///GridView
 class PhotosList extends StatelessWidget {
   final List<Photo> photos;
 
@@ -83,6 +101,7 @@ class PhotosList extends StatelessWidget {
   }
 }
 
+///Photo实体类
 class Photo {
   final int albumId;
   final int id;
--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 import 'package:startup_namer/gesture.dart';
 import 'http/http_delete.dart';
-import 'http/http_isolate.dart';
+import 'http/http_gridview.dart';
 import 'http/http_network_update.dart';
 import 'http/web_socket.dart';
 import 'provider/home_page.dart';
@@ -23,7 +23,7 @@ import 'anim/anim.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(UpdateApp());
+void main() => runApp(GirdApp());
 
 // void main() => runApp(ChangeNotifierProvider<MyCounter>.value(
 //       // notifier: MyCounter(1),
