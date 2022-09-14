023d29d047bdbd19caf6e74c91ead183eaf0eb58
hongri <1285052668@qq.com>
2022-09-14 11:53:54 +0800

    Native页面跳转到Flutter页面

---
+++ android/app/src/main/java/com/hongri/startup_namer/HomeActivity.kt
@@ -0,0 +1,24 @@
+package com.hongri.startup_namer
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_home.*
+
+class HomeActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        jumpToFlutterBtn.setOnClickListener {
+            fromNativeJump2Flutter();
+        }
+    }
+
+    private fun fromNativeJump2Flutter() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("name", "yao")
+        intent.putExtra("age", 18)
+        startActivity(intent)
+    }
+}
