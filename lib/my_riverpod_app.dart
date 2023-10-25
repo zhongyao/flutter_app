e993ac2fbcb6e3a3c7cee45b1e3d0efd30aed59b
hongri <1285052668@qq.com>
2023-10-25 10:52:20 +0800

    test push record

--- lib/my_riverpod_app.dart
+++ lib/my_riverpod_app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:startup_namer/page/riverpod_page.dart';
 
-///RiverPod App
+///RiverPod App..
 class MyRiverPodApp extends StatelessWidget {
   const MyRiverPodApp({super.key});
 
